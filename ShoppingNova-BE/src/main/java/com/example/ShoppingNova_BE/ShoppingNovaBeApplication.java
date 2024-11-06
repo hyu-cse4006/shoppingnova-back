3cbf0e751284c8f561aafc3cc907e9e57ece4a85
홍성문 <2020041412@hanyang.ac.kr>
2024-11-06 17:50:46 +0900

    init

---
+++ ShoppingNova-BE/src/main/java/com/example/ShoppingNova_BE/ShoppingNovaBeApplication.java
@@ -0,0 +1,11 @@
+package com.example.ShoppingNova_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingNovaBeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingNovaBeApplication.class, args);
+	}
+}
